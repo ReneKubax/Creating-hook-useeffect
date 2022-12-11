0cf26935c15e48afe8fe73aefeb4b638d6cb7d85
ReneKubax <97212849+ReneKubax@users.noreply.github.com>
2022-12-11 16:40:44 -0600

    Creando hook

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { useFetch } from './hooks/useFetch';
+
+
+const App = () => {
+
+ const [counter, setCounter] = useState(0);
+
+ const {data, loading, error} = useFetch("https://jsonplaceholder.typicode.com/users")
+
+ if(loading) return <div>Cargando...</div>
+ if(error) return <div>{error}</div>
+
+  return (
+    <>
+    <h1> UseEffect </h1>
+    <button onClick={() => setCounter(counter + 1)}>Counter: {counter}</button>
+    <ul>
+    {
+      data.map(data => (
+        <li key={data.id}>{data.name}</li>
+      ))
+    }
+    </ul>
+    </>
+  )
+}
+
+export default App
